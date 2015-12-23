bcb0959002d915145f723daf8dc2d583903826d0
John-Marc Chandonia <JMChandonia@lbl.gov>
2015-12-23 19:59:52 +0000

    Added code to get contig information for genome

--- lib/src/rbtnseq/TnSeq.java
+++ lib/src/rbtnseq/TnSeq.java
@@ -36,7 +36,6 @@ public class TnSeq {
         rv.setAuthAllowedForHttp(true);
         return rv;
     }
-    
 
     /**
        dumps a (single end) reads object out in fastQ format.  
@@ -71,10 +70,66 @@ public class TnSeq {
             pb.start().waitFor();
         }
         catch (Exception e) {
+            System.out.println("Error reading single-end library as assembly object");
+            // should try to read as KBaseFile.SingleEndLibrary here
             e.printStackTrace();
+            // fastQFile.delete(); save for debugging
+            fastQFile = null;
         }
 
         return fastQFile;
+    }
+
+
+    /**
+       dumps a Genome object out in FEBA directory format; returns
+       name of genome directory
+    */
+    public static File dumpGenomeTab(String wsURL,
+                                     AuthToken token,
+                                     File tempDir,
+                                     String ws,
+                                     String genomeRef) throws Exception {
+        WorkspaceClient wc = createWsClient(wsURL,token);
+        ObjectMapper mapper = new ObjectMapper();
+
+        File genomeDir = File.createTempFile("g", "", tempDir);
+        genomeDir.delete();
+        genomeDir.mkdir();
+
+        try {
+            Genome genome = wc.getObjects(Arrays.asList(new ObjectIdentity().withRef(ws+"/"+genomeRef))).get(0).getData().asClassInstance(Genome.class);
+            if (genome==null)
+                throw new Exception("Null return error reading genome");
+
+            // make sure we can read contigset also:
+            String contigsetRef = genome.getContigsetRef();
+            if (contigsetRef == null)
+                throw new Exception("ContigSet not defined for this genome");
+            ContigSet cs = wc.getObjects(Arrays.asList(new ObjectIdentity().withRef(contigsetRef))).get(0).getData().asClassInstance(ContigSet.class);
+            HashMap<String,String> contigSeqs = new HashMap<String,String>();
+            for (Contig c : cs.getContigs())
+                contigSeqs.put(c.getId(),c.getSequence());
+            cs = null;
+
+            // genome.fna = nucleic acid seq for each contig
+            List<String> contigs = genome.getContigIds();
+            if (contigs==null)
+                throw new Exception("No contig IDs defined for this genome");
+            int nContigs = contigs.size();
+            for (int i=0; i<nContigs; i++) {
+                String contigID = contigs.get(i);
+                String seq = contigSeqs.get(contigID);
+            }
+            
+        }
+        catch (Exception e) {
+            System.out.println("Error reading genome");
+            e.printStackTrace();
+            genomeDir = null;
+        }
+        
+        return genomeDir;
     }
     
     /**
